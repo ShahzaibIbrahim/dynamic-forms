56f3c25b141ba0b8fdd1b4d33d675d1d86e0fa1f
Shahzaib Ibrahim <shahzaib.ibrahim@forwardyou.com>
2022-05-06 18:38:58 +0500

    Adding Ok Button For Error List

--- src/components/Controls/ErrorList.js
+++ src/components/Controls/ErrorList.js
@@ -5,48 +5,51 @@ import {
   ListItemText,
   Typography,
   ListItemButton,
-  Divider
+  Divider,
+  Button,
+  Grid
 } from "@mui/material";
 import ArrowRightIcon from "@mui/icons-material/ArrowRight";
-import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
 import AppModal from "../UI/Modal/AppModal";
-import ScrollIntoView from 'react-scroll-into-view'
+import ScrollIntoView from "react-scroll-into-view";
 
 const ErrorList = (props) => {
   const errorList = props.errorList;
-  
+
   return (
     <AppModal open={props.modalIsOpen} handleClose={props.handleModalClose}>
-      <Typography gutterBottom variant="h5" component="div" >
-        <ErrorOutlineIcon fontSize="large"/> {errorList.length} Errors
+      <Typography gutterBottom variant="h5" component="div">
+        <ErrorOutlineIcon fontSize="large" /> {errorList.length} Errors
       </Typography>
       <Divider />
       <List dense={true}>
         {errorList &&
           errorList.map((list) => (
             <div key={list.fieldId}>
-              <ScrollIntoView smooth onClick={props.handleModalClose} selector={'#'+ list.fieldId}>
-              <ListItem sx={{ margin: 0.01 }}>
-                <ListItemButton>
-                  <ListItemIcon>
-                    <ArrowRightIcon />
-                  </ListItemIcon>
-                  <ListItemText primary={list.errorMessage} />
-                </ListItemButton>
-              </ListItem>
-              
+              <ScrollIntoView
+                smooth
+                onClick={props.handleModalClose}
+                selector={"#" + list.fieldId}
+              >
+                <ListItem sx={{ margin: 0.01 }}>
+                  <ListItemButton>
+                    <ListItemIcon>
+                      <ArrowRightIcon />
+                    </ListItemIcon>
+                    <ListItemText primary={list.errorMessage} />
+                  </ListItemButton>
+                </ListItem>
               </ScrollIntoView>
             </div>
           ))}
       </List>
+      <Divider light />
+      <Grid item xs={12}>
+          <Button variant="outlined" fullWidth onClick={props.handleModalClose}>Ok</Button>
+      </Grid>
     </AppModal>
   );
 };
-
-/* secondaryAction={
-     <IconButton edge="end" aria-label="delete">
-       <DeleteIcon />
-     </IconButton>
-   } */
 
 export default ErrorList;
